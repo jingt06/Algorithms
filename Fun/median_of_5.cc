cb3fbe500173fc9d3a08b853146f8a6523480138
Jing Tao <genetao06@hotmail.com>
2015-12-02 22:26:17 -0500

    median of 5 algorithm

---
+++ Fun/median_of_5.cc
@@ -0,0 +1,26 @@
+using namespace std;
+
+
+//the worst case comparision is minimun, which is 6 times
+int median_of_5 ( int a , int b , int c , int d , int e){
+	if(b < a)        
+		swap(a, b);        // a < b
+	if(d < c)
+		swap(c, d);        // a < b , c < d
+	if(c < a){
+		swap(c, a);
+		swap(b, d);        // a < b , a < c , c < d
+	}
+	swap(a,e);             // a , b , c < d ( e out )
+	if(b < a)
+		swap(a, b);        // a < b , c < d
+	if(c < a){
+		swap(c, a);
+		swap(b, d);        // a < b , c < d , a < c ( a out )
+	}                            // c < d, b
+	if(b < c){
+		return b; 
+	}else{
+		return c;
+	}
+}
